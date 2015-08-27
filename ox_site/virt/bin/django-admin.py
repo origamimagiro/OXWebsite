fbe2b43c8c83f1d6eef90b57f173d7ae44a562bf
Henry Aspegren <henrya@mit.edu>
2015-08-27 14:31:17 -0400

    Cleaning up the repo (removing PYC files and virtual environment)

--- ox_site/virt/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/henryaspegren/Documents/Projects/OXWebsite/ox_site/virt/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

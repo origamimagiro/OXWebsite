6d03c09ceb1d19dd685df7bb3aa53f4fda4fa2f5
Henry Aspegren <henrya@mit.edu>
2015-09-16 22:16:20 -0400

    test

--- ox_site/settings.py
+++ ox_site/settings.py
@@ -84,7 +84,7 @@ STATICFILES_DIRS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = ''
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 

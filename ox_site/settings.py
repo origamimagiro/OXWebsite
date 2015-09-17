62f23c783918128a93056e8a2918d723a5145fb9
Henry Aspegren <henrya@mit.edu>
2015-09-17 17:07:35 -0400

    pull from environment variables

--- ox_site/settings.py
+++ ox_site/settings.py
@@ -85,7 +85,7 @@ STATIC_URL = 'http://s3.amazonaws.com/oxwebsite/'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = '<Y NAME>'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'

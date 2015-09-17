b65de178fce5eeb650d6b626d1fae7e60d040f16
Henry Aspegren <henrya@mit.edu>
2015-09-16 22:24:50 -0400

    removed static file development serving

--- ox_site/urls.py
+++ ox_site/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     url(r'^history/', views.history)
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

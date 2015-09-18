9c699d383e4046f1e573d6fdcbeff7f84c84b607
Henry Aspegren <henrya@mit.edu>
2015-09-17 22:04:32 -0400

    using original config

--- s3utils.py
+++ s3utils.py
@@ -1,13 +1,4 @@
 from storages.backends.s3boto import S3BotoStorage
-from ox_site import settings
 
-
-class FixedS3BotoStorage(S3BotoStorage):
-    def url(self, name):
-        url = super(FixedS3BotoStorage, self).url(name)
-        if name.endswith('/') and not url.endswith('/'):
-            url += '/'
-        return url
-
-StaticRootS3BotoStorage = lambda: FixedS3BotoStorage(location=settings.STATIC_URL)
-MediaRootS3BotoStorage  = lambda: FixedS3BotoStorage(location=settings.MEDIA_URL)
+StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')

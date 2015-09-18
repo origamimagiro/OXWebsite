cde5473cdf85c0b4e53e92226a92223809b5d7c8
Henry Aspegren <henrya@mit.edu>
2015-09-17 23:00:55 -0400

    added django-imagekit and removed default value

--- ox_site/settings.py
+++ ox_site/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'people',
-    'storages'
+    'storages',
+    'imagekits'
 )
 
 MIDDLEWARE_CLASSES = (
--- people/models.py
+++ people/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.admin import User
 from ox_site import settings
-
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 class Brother(models.Model):
 	user = models.OneToOneField(User, unique=True) #leverage the base user model
-	image = models.ImageField(default='/images/coatarms.jpg')
 	class_year = models.IntegerField(blank=True)
 	major = models.CharField(max_length=20,blank=True, default="undecided")
 	hometown = models.CharField(max_length=100, blank=True)
 	about = models.CharField(max_length=500, blank=True)
 	campus_involvement = models.CharField(max_length=500, blank=True)
 	is_alum = models.BooleanField(default=False)
+	image = models.ImageField()
+    image_headshot = ImageSpecField(processors=[ResizeToFill(300, 400)], format='JPEG')
 	big_brother = models.ManyToManyField('self', blank=True) # Many to Many with self
 
 
